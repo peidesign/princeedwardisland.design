54a08e6ed6755f9096173958e1030f454715381b
Chris Macrae <hello@chrisdmacrae.com>
2018-03-26 00:38:23 -0300

    Fix button

--- src/components/button/shadow/index.js
+++ src/components/button/shadow/index.js
@@ -3,19 +3,20 @@ import Link from "../../link";
 import styles from "./shadow.module.css";
 
 export default ({ children, href, to, external, color, className }) => (
-  <Link href={href} to={to} external={external}>
+  <Link
+    href={href}
+    to={to}
+    external={external}
+    className={`${className} ${styles.buttonShadow} inline-block text-${
+      color ? color : "purple"
+    }-pale`}
+  >
     <div
-      className={`${className} ${styles.buttonShadow} inline-block text-${
+      className={`bg-${
         color ? color : "purple"
-      }-pale`}
+      } hover:opacity-90 font-strong inline-block pt-4 pr-8 pl-8 pb-4 text-lg text-white w-auto`}
     >
-      <div
-        className={`bg-${
-          color ? color : "purple"
-        } hover:opacity-90 font-strong inline-block pt-4 pr-8 pl-8 pb-4 text-lg text-white w-auto`}
-      >
-        {children}
-      </div>
+      {children}
     </div>
   </Link>
 );
