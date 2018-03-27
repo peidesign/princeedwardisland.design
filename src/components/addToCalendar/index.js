8bb2cecb1f0e127707d84c2d8d6bbc0380ee572a
Chris Macrae <hello@chrisdmacrae.com>
2018-03-26 21:41:08 -0300

    Cleanup meetup info

--- src/components/addToCalendar/index.js
+++ src/components/addToCalendar/index.js
@@ -1,23 +1,17 @@
 import React from "react";
 import AddToCalendar from "react-add-to-calendar";
-import styles from "./addToCalendar.module.css"
+import styles from "./addToCalendar.module.css";
 
-export default () => {
-  const event = {
-    title: "Sample Event",
-    description: "This is the sample event provided as an example only",
-    location: "Portland, OR",
-    startTime: "2016-09-16T20:15:00-04:00",
-    endTime: "2016-09-16T21:45:00-04:00"
-  };
-
+export default ({ event }) => {
   const classes = {
     rootClass: "relative",
     buttonWrapperClass:
       "cursor-pointer inline-block text-purple hover:opacity-75",
     buttonClassClosed: "",
     buttonClassOpen: "",
-    dropdownClass: `${styles.dropdown} absolute bg-white border border-purple-pale mt-2 p-4 pin-top rounded text-purple-pale w-64 z-50`
+    dropdownClass: `${
+      styles.dropdown
+    } absolute bg-white border border-purple-pale mt-2 p-4 pin-top rounded text-purple-pale w-64 z-50`
   };
 
   return (
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import AddToCalendar from "../components/addToCalendar"
+import moment from "moment"
+import AddToCalendar from "../components/addToCalendar";
 import Button from "../components/button";
 import Footer from "../components/footer";
 import Hero from "../components/hero";
@@ -7,16 +8,27 @@ import Link from "../components/link";
 import Shapes from "../components/shapes";
 
 export default ({ data }) => {
+  const upcomingEvent = {
+    title: "Introduction to Human-centered Design",
+    description:
+      "For the first official meetup on human-centered design, we have a keynote speaker covering the basics of human-centered design, and a short workshop on executing human-centered design principles for your own projects!",
+    location: "Atlantic Technology Centre, 90 University Avenue, Charlottetown",
+    startTime: "2018-04-24T20:19:00-04:00",
+    endTime: "2018-04-24T21:30:00-04:00"
+  };
+  
+  const startDate = moment(upcomingEvent.startTime).format("MMMM DD, YYYY")
+
   const heroCta = (
     <div>
-      <p className="text-2xl">Our next meetup is April 11, 2018</p>
+      <p className="text-2xl">Our next meetup is {startDate}</p>
       <Button
         href="https://www.meetup.com/pei-designers/"
         external={true}
         style="shadow"
         className="mt-4 mb-4"
       >
-        Sign up for updates
+        RSVP today!
       </Button>
       <p className="text-sm">
         <AddToCalendar />
@@ -39,7 +51,9 @@ export default ({ data }) => {
         <div className="sm:flex sm:flex-row m-auto max-w-2xl">
           <div className="sm:w-1/2 flex flex-col">
             <h2 className="text-white">💬 Join the conversation on Slack</h2>
-            <p className="mt-4 opacity-75">Discuss design, find jobs, and meet new people.</p>
+            <p className="mt-4 opacity-75">
+              Discuss design, find jobs, and meet new people.
+            </p>
             <div className="mt-auto">
               <Button
                 href="http://bit.ly/peislack"
